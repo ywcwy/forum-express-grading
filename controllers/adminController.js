7e10ebb89609311e50d16139ae6a82b6df58d245
yuwen chang <provence5058@gmail.com>
2021-02-02 12:59:06 +0800

    feat: add admin restaurant create form & post feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,6 +4,19 @@ const adminController = {
   getRestaurants: (req, res) => {
     Restaurant.findAll({ raw: true })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
+  },
+  createRestaurant: (req, res) => res.render('admin/create'),
+  postRestaurant: (req, res) => {
+    const { name, tel, address, opening_hours, description } = req.body
+    if (!req.body) {
+      req.flash('error_messages', '請填寫所有欄位。')
+      return res.redirect('back')
+    }
+    return Restaurant.create({ name, tel, address, opening_hours, description })
+      .then(() => {
+        req.flash('success_messages', '餐廳新增成功。')
+        res.redirect('/admin/restaurants')
+      })
   }
 }
 
