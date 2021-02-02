2311832fdb5606a49b0bee1f489ec97f6d96f5d0
yuwen chang <provence5058@gmail.com>
2021-02-02 18:49:14 +0800

    feat: install multer and add image upload feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const db = require('../models')
 const Restaurant = db.Restaurant
 const adminController = {
@@ -12,11 +13,25 @@ const adminController = {
       req.flash('error_messages', '請填寫餐廳名稱。')
       return res.redirect('back')
     }
-    return Restaurant.create({ name, tel, address, opening_hours, description })
-      .then(() => {
-        req.flash('success_messages', '餐廳新增成功。')
-        res.redirect('/admin/restaurants')
+    const { file } = req
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          return Restaurant.create({ name, tel, address, opening_hours, description, image: file ? `/upload/${file.originalname}` : null })
+            .then(() => {
+              req.flash('success_messages', '餐廳新增成功。')
+              return res.redirect('/admin/restaurants')
+            })
+        })
       })
+    } else {
+      return Restaurant.create({ name, tel, address, opening_hours, description, image: null })
+        .then(() => {
+          req.flash('success_messages', '餐廳新增成功。')
+          return res.redirect('/admin/restaurants')
+        })
+    }
   },
   getRestaurant: (req, res) => {
     Restaurant.findByPk(req.params.id, { raw: true })
@@ -32,12 +47,31 @@ const adminController = {
       req.flash('error_messages', '請填寫餐廳名稱。')
       return res.redirect('back')
     }
-    return Restaurant.findByPk(req.params.id)
-      .then(restaurant => {
-        restaurant.update({ name, tel, address, opening_hours, description })
-        req.flash('success_messages', '餐廳新增成功。')
-        res.redirect('/admin/restaurants')
+    const { file } = req
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          return Restaurant.findByPk(req.params.id)
+            .then((restaurant) => {
+              restaurant.update({ name, tel, address, opening_hours, description, image: file ? `/upload/${file.originalname}` : restaurant.image })
+                .then(() => {
+                  req.flash('success_messages', '餐廳更新成功。')
+                  res.redirect('/admin/restaurants')
+                })
+            })
+        })
       })
+    } else {
+      return Restaurant.findByPk(req.params.id)
+        .then(restaurant => {
+          restaurant.update({ name, tel, address, opening_hours, description, image: restaurant.image })
+            .then(() => {
+              req.flash('success_messages', '餐廳更新成功。')
+              res.redirect('/admin/restaurants')
+            })
+        })
+    }
   },
   deleteRestaurant: (req, res) => {
     Restaurant.findByPk(req.params.id)
