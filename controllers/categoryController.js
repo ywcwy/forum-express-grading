a1cb29a0f7b37395d6f955f705053a278b35774e
yuwen chang <provence5058@gmail.com>
2021-02-06 15:19:01 +0800

    feat: add admin categories post feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,6 +5,15 @@ const categoryController = {
   getCategories: (req, res) => {
     Category.findAll({ raw: true, nest: true })
       .then(categories => res.render('admin/categories', { categories }))
+  },
+  postCategory: (req, res) => {
+    const { name } = req.body
+    if (!name) {
+      req.flash('error_messages', '請新增分類名稱')
+      return res.redirect('back')
+    }
+    return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -34,6 +34,7 @@ module.exports = (app, passport) => {
   app.put('/admin/users/:id/toggleAdmin', authenticatedAdmin, adminController.toggleAdmin)
 
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
+  app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
