9ff05ebfac631dd98bd5567c2f2c3ba25ab716db
yuwen chang <provence5058@gmail.com>
2021-02-08 12:27:52 +0800

    feat: add ddelete comment feature for admin

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,13 +2,16 @@ const db = require('../models')
 const Comment = db.Comment
 const commentController = {
   postComment: (req, res) => {
-    console.log(req.body)
     Comment.create({
       text: req.body.text,
       RestaurantId: req.body.restaurantId, // 因為 form 內有設計 hidden 欄位存放 restaurantId
       UserId: req.user.id
     })
       .then(() => res.redirect(`/restaurants/${req.body.restaurantId}`))
+  },
+  deleteComment: (req, res) => {
+    Comment.findByPk(req.params.id)
+      .then(comment => comment.destroy().then(() => res.redirect(`/restaurants/${comment.RestaurantId}`)))
   }
 }
 module.exports = commentController
