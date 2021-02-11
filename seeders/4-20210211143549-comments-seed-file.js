6233e4fd3c295248bda835de0b025beb03c28297
yuwen chang <provence5058@gmail.com>
2021-02-12 00:09:41 +0800

    modify restaurants' id in comment-seed-file

--- seeders/4-20210211143549-comments-seed-file.js
+++ seeders/4-20210211143549-comments-seed-file.js
@@ -4,37 +4,37 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Comments', [{
       text: 'nice',
-      RestaurantId: 3,
+      RestaurantId: 1,
       UserId: 11,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       text: '五星好評',
-      RestaurantId: 3,
+      RestaurantId: 11,
       UserId: 11,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       text: '還OK啦',
-      RestaurantId: 9,
+      RestaurantId: 11,
       UserId: 11,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       text: '環境油膩',
-      RestaurantId: 18,
+      RestaurantId: 21,
       UserId: 11,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       text: 'great',
-      RestaurantId: 37,
+      RestaurantId: 31,
       UserId: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       text: '非常普通',
-      RestaurantId: 44,
+      RestaurantId: 41,
       UserId: 11,
       createdAt: new Date(),
       updatedAt: new Date()
